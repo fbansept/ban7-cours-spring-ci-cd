db8542d755b93a904f3f520885eaf4a092a9fa97
fbansept <bansept.franck@gmail.com>
2025-03-19 11:38:23 +0100

    test jwt secret

--- src/main/java/edu/fbansept/ban7coursspringcicd/HelloController.java
+++ src/main/java/edu/fbansept/ban7coursspringcicd/HelloController.java
@@ -1,5 +1,6 @@
 package edu.fbansept.ban7coursspringcicd;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class HelloController {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello world";
+        return "Hello " + jwtSecret;
     }
 
 }
